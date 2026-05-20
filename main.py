3f4a377a88ae3d08b9d4534a89e91eb0484ab63e
dg2026811 <dg2026811@danggok.hs.kr>
2026-05-20 11:10:26 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+Web VPython 3.2
+sphere(opacity=1, pos=vec(-3,3,-1),radius=5,color=vec(190/255,164/255,60/255))
+sphere(opacity=1, pos=vec(-1,5,3),radius=1,color=vec(255/255,255/255,255/255))
+sphere(opacity=1, pos=vec(1,5,1.5),radius=1,color=vec(255/255,255/255,255/255))
+sphere(opacity=1, pos=vec(-4,7.5,3),radius=0.3,color=vec(0/255,0/255,0/255))
